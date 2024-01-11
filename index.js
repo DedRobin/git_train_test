26857ab94b9c3d6127fec43fe0524f91da4d77ab
DedRobin <pavelrabekin@gmail.com>
2024-01-11 11:30:52 +0300

    delete all files

--- index.js
+++
@@ -1,9 +0,0 @@
-const progressBar = document.querySelector('.progress_bar');
-const counter = document.getElementById('counter');
-
-progressBar.addEventListener('animationiteration', toCountIteration);
-
-function toCountIteration() {
-    const num = +counter.lastChild.innerHTML;
-    counter.lastChild.innerHTML = num + 1;
-}
